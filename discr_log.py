2ab01bda55fdcf830c0636433bf97dfe250c0bd7
Berendei-Jr <vedvedved2003@gmail.com>
2025-11-09 13:10:25 +0300

    delete discr_log

--- discr_log.py
+++ discr_log.py
@@ -1,44 +1,62 @@
+def pow(a, n, m):
+    return a**n%m
+
+def urovn(osn, base, arr, res, mod):
+    l = len(arr)
+    print("2**(", end = "")
+    for i in range(l+1):
+        print(f"x{i}*2**{i}", end = "")
+        print(" ... )")
+    stepen = (mod-1)//(base**(l+1))
+    stepen_arr = [stepen*base**i for i in range(l+1)]
+    for i in range(l):
+        stepen_arr[i] *= arr[i]
+    
+
+    
+    left_pow = stepen_arr[-1]
+    right_pow = [0]+[mod-i-1 for i in stepen_arr[:-1]]
+    #right = sum([pow(osn, i, mod) for i in right_pow]) * pow(res, stepen, mod)
+    right =  pow(osn, sum(right_pow), mod) * pow(res, stepen, mod)
+    right = right % mod
+    stepen_arr[-1] = f"{stepen_arr[-1]}x"
+    
+    print(f"{osn}**{left_pow}x={right}")
+    for i in range(base):
+        if pow(osn, left_pow*i, mod) == right:
+            x = i
+            break
+    else:
+        print("x not found; error")
+        return
+    
+    print(f"{x = }")
+    return x
+
+
 BASE = 2
-TARGET = 11
+TARGET = 15
 MOD = 37
 
-MNOJITELI = [[2, 4], [3, 9]]
+MNOJITELI = [[2, 2], [3, 2]]
 
+d= dict()
 
-for mnojitel in MNOJITELI:
-    razlozhenie_base = mnojitel[0]
-    print(f"Множитель {razlozhenie_base}")
+for p, n in MNOJITELI:
+    x_arr = []
+    for i in range(n):
+        x = urovn(BASE, p, x_arr, TARGET, MOD)
+        print()
+        x_arr += [x]
+    
+    d[p] = x_arr
 
-    power = int((MOD - 1) / razlozhenie_base)
-
-    a = (TARGET ** power) % MOD
-
-    print(f"{BASE}^({power}*X0) = {a}")
-
-    if a == 1:
-        x0 = 0
+print(d)
+for i in range(MOD):
+    valid = 1
+    for p, arr in d.items():
+        m = sum([p**j*arr[j] for j in range(len(arr))])
+        if i%(p**(len(arr))) != m: break
     else:
-        for x in range(razlozhenie_base):
-            if BASE ** (power * x) % MOD == a:
-                x0 = x
+        print(i)
 
-    print(f"X0 = {x0}\n")
-
-    second_mnoj = mnojitel[1]
-
-    print(f"Множитель {second_mnoj}")
-    power = int((MOD - 1) / second_mnoj)
-    a = (TARGET ** power) % MOD
-    print(f"{BASE}^({power}*X0 + {razlozhenie_base*power}*X1) = {a}")
-
-    if x0 == 0:
-        if a == 1:
-            x1 = 0
-        else:
-            for x in range(second_mnoj):
-                if BASE ** (razlozhenie_base*power * x) % MOD == a:
-                    x1 = x
-    else:
-        x1 = (a - BASE ** (power * x0)) // BASE ** (power * razlozhenie_base * x0)
-
-    print(f"X1 = {x1}\n")
